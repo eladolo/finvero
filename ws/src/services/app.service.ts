ee25ba3a3525872eac409cf667e2bb088949aa24
Adolfer Valle Canales <adolito@hotmail.com>
2023-11-17 23:26:05 -0600

    CHG - Add default files and default state

--- ws/src/services/app.service.ts
+++ ws/src/services/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'REST API 📟';
+        return 'Finvero REST API ⚙️ 📟';
     }
 }
